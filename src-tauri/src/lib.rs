527f7efe0ab84adfb50d213c1340e9553dea224b
Alan Smith <commit@alanwsmith.com>
2026-09-14 07:29:16 -0400

    working to use webview to embed youtube home page

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,15 +1,34 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+  format!(
+    "Hello, {}! You've been greeted from Rust!",
+    name
+  )
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-    tauri::Builder::default()
-        .plugin(tauri_plugin_global_shortcut::Builder::new().build())
-        .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+  tauri::Builder::default()
+    .setup(|app| {
+      let webview_window =
+        tauri::WebviewWindowBuilder::new(
+          app,
+          "label",
+          tauri::WebviewUrl::App(
+            "https://www.youtube.com".into(),
+          ),
+        )
+        .build()?;
+      Ok(())
+    })
+    .run(tauri::generate_context!())
+    .expect("error while running tauri application");
+
+  // tauri::Builder::default()
+  //     .plugin(tauri_plugin_global_shortcut::Builder::new().build())
+  //     .plugin(tauri_plugin_opener::init())
+  //     .invoke_handler(tauri::generate_handler![greet])
+  //     .run(tauri::generate_context!())
+  //     .expect("error while running tauri application");
 }
--- src/main.js
+++ src/main.js
@@ -1,10 +1,61 @@
-const { invoke } = window.__TAURI__.core;
-const { register } = window.__TAURI__.globalShortcut;
+import { Window } from "@tauri-apps/api/window"
+import { Webview } from "@tauri-apps/api/webview"
 
-await register("CommandOrControl+Shift+C", () => {
-    console.log("Shortcut triggered");
-    document.querySelector("#status").innerHTML = new Date();
+const appWindow = new Window('uniqueLabel');
+
+appWindow.once('tauri://created', async function () {
+  // `new Webview` Should be called after the window is successfully created,
+  // or webview may not be attached to the window since window is not created yet.
+
+  // // loading embedded asset:
+  // const webview = new Webview(appWindow, 'theUniqueLabel', {
+  //   url: 'path/to/page.html',
+  //   // create a webview with specific logical position and size
+  //   x: 0,
+  //   y: 0,
+  //   width: 800,
+  //   height: 600,
+  // });
+
+  // alternatively, load a remote URL:
+  const webview = new Webview(appWindow, 'theUniqueLabel', {
+    url: 'https://github.com/tauri-apps/tauri',
+
+    // create a webview with specific logical position and size
+    x: 0,
+    y: 0,
+    width: 800,
+    height: 600,
+  });
+
+  webview.once('tauri://created', function () {
+    // webview successfully created
+  });
+  webview.once('tauri://error', function (e) {
+    // an error happened creating the webview
+  });
+
+  // emit an event to the backend
+  await webview.emit("some-event", "data");
+  // listen to an event from the backend
+  const unlisten = await webview.listen("event-name", e => { });
+  unlisten();
 });
+
+
+// const { invoke } = window.__TAURI__.core;
+// const { register } = window.__TAURI__.globalShortcut;
+
+// await register("CommandOrControl+Shift+C", () => {
+//     //console.log("Shortcut triggered");
+//     document.querySelector("#videoFrame").contentWindow.postMesage(
+// 	{
+// 	    type: "playpause", data: {}
+// 	},
+// 	'al9000.com'
+//     );
+//     document.querySelector("#status").innerHTML = new Date();
+// });
 
 /*
 window.addEventListener("DOMContentLoaded", () => {
