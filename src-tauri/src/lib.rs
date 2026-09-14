97af0ba16249ca4d631f45cfbfa1fdf7af790ea7
Alan Smith <commit@alanwsmith.com>
2026-09-14 08:04:29 -0400

    Basic global hotkey set up for webview

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,25 +1,65 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-#[tauri::command]
-fn greet(name: &str) -> String {
-  format!(
-    "Hello, {}! You've been greeted from Rust!",
-    name
-  )
+use tauri::{command, AppHandle};
+
+#[command]
+fn do_rewind(app: AppHandle) {
+  dbg!("ASD");
+  println!("eeeee");
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
   tauri::Builder::default()
+    // .plugin(
+    //   tauri_plugin_global_shortcut::Builder::new()
+    //     .build(),
+    // )
+    // .plugin(tauri_plugin_opener::init())
     .setup(|app| {
+      use tauri_plugin_global_shortcut::{
+        Code, GlobalShortcutExt, Modifiers, Shortcut,
+        ShortcutState,
+      };
+
+      let ctrl_n_shortcut = Shortcut::new(
+        Some(Modifiers::CONTROL),
+        Code::KeyN,
+      );
+      app.handle().plugin(
+        tauri_plugin_global_shortcut::Builder::new()
+          .with_handler(move |_app, shortcut, event| {
+            println!("{:?}", shortcut);
+            if shortcut == &ctrl_n_shortcut {
+              match event.state() {
+                ShortcutState::Pressed => {
+                  println!("Ctrl-N Pressed!");
+                }
+                ShortcutState::Released => {
+                  println!("Ctrl-N Released!");
+                }
+              }
+            }
+          })
+          .build(),
+      )?;
+
+      app
+        .global_shortcut()
+        .register(ctrl_n_shortcut)?;
+
       let webview_window =
         tauri::WebviewWindowBuilder::new(
           app,
-          "label",
+          "main",
           tauri::WebviewUrl::App(
             "https://www.youtube.com".into(),
           ),
         )
         .build()?;
+      // webview_window
+      //   .eval(r#"console.log("HEREREREERE""#)?;
+
+      webview_window.open_devtools();
+
       Ok(())
     })
     .run(tauri::generate_context!())
@@ -32,3 +72,12 @@ pub fn run() {
   //     .run(tauri::generate_context!())
   //     .expect("error while running tauri application");
 }
+
+// // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+// #[tauri::command]
+// fn greet(name: &str) -> String {
+//   format!(
+//     "Hello, {}! You've been greeted from Rust!",
+//     name
+//   )
+// }
