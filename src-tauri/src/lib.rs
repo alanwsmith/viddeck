a245ebde5c6674608418d5789a0ec60ed3f21253
Alan Smith <commit@alanwsmith.com>
2026-09-15 14:36:54 -0400

    Added rewind key

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -21,38 +21,33 @@ pub fn run() {
       webview_window.eval(
         r#"
 function awsPlayPause() {
+  console.log("awsPlayPause()");
   let vid = document.querySelector("video");
   if (vid) {
     if (vid.paused) {
-      if (vid.currentTime > 10) {
-        vid.currentTime -= 7;
-      }
+      vid.currentTime = Math.max(0, vid.currentTime - 7);
       vid.play();
     }
     else {
       vid.pause()
     }
   }
-}
+};
 
 function awsRewind() {
+  console.log("awsRewind()");
   let vid = document.querySelector("video");
   if (vid) {
-    if (vid.currentTime > 13) {
-      vid.currentTime -= 10;
-    }
-    else {
-      vid.currentTime = 0;
-    }
+    vid.currentTime = Math.max(0, vid.currentTime - 10);
   }
-}
+};
 
 function awsFastForward() {
   let vid = document.querySelector("video");
   if (vid) {
     vid.currentTime += 8;
   }
-}
+};
 
 
 function awsPlayFaster() {
@@ -62,7 +57,7 @@ function awsPlayFaster() {
       vid.playbackRate += 0.25;
     }
   }
-}
+};
 
 
 function awsPlaySlower() {
@@ -72,7 +67,7 @@ function awsPlaySlower() {
       vid.playbackRate -= 0.25;
     }
   }
-}
+};
 
 "#,
       )?;
@@ -85,45 +80,56 @@ function awsPlaySlower() {
         Code::KeyS,
       );
 
+      let rewind_key = Shortcut::new(
+        Some(Modifiers::ALT | Modifiers::SHIFT),
+        Code::KeyA,
+      );
+
+
+      // let fast_forward_key = Shortcut::new(
+      //   Some(Modifiers::ALT | Modifiers::SHIFT),
+      //   Code::KeyD,
+      // );
+
+      // let faster_key = Shortcut::new(
+      //   Some(Modifiers::ALT | Modifiers::SHIFT),
+      //   Code::KeyC,
+      // );
+
+      // let slower_key = Shortcut::new(
+      //   Some(Modifiers::ALT | Modifiers::SHIFT),
+      //   Code::KeyZ,
+      // );
+
       app.handle().plugin(
         tauri_plugin_global_shortcut::Builder::new()
           .with_handler(move |appx, shortcut, event| {
-            if shortcut == &play_pause_key {
               if let ShortcutState::Pressed =
-                event.state()
-              {
-                appx
-                  .get_webview_window("main")
-                  .expect("no main widow")
-                  .eval(r#"awsPlayPause();"#)
-                  .expect("eval did not work");
-              }
-              // match event.state() {
-              //   ShortcutState::Pressed => {
-              //     let wv = appx
-              //       .get_webview_window("main")
-              //       .expect("no main widow");
-              //     wv.eval(r#"awsPlayPause();"#)
-              //       .expect("eval did not work");
-              //   }
-              //   _ => (),
-              // }
-            }
+                event.state() {
+                    if shortcut == &play_pause_key {
+                        appx
+                            .get_webview_window("main")
+                            .expect("no main widow")
+                            .eval(r#"awsPlayPause();"#)
+                            .expect("eval did not work");
+                    }
+                    if shortcut == &rewind_key {
+                        appx
+                            .get_webview_window("main")
+                            .expect("no main widow")
+                            .eval(r#"awsRewind();"#)
+                            .expect("eval did not work");
+                    }
+                }
           })
           .build(),
       )?;
       app.global_shortcut().register(play_pause_key)?;
+      app.global_shortcut().register(rewind_key)?;
       Ok(())
     })
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
-
-  // tauri::Builder::default()
-  //     .plugin(tauri_plugin_global_shortcut::Builder::new().build())
-  //     .plugin(tauri_plugin_opener::init())
-  //     .invoke_handler(tauri::generate_handler![greet])
-  //     .run(tauri::generate_context!())
-  //     .expect("error while running tauri application");
 }
 
 // // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
@@ -134,3 +140,21 @@ function awsPlaySlower() {
 //     name
 //   )
 // }
+
+// match event.state() {
+//   ShortcutState::Pressed => {
+//     let wv = appx
+//       .get_webview_window("main")
+//       .expect("no main widow");
+//     wv.eval(r#"awsPlayPause();"#)
+//       .expect("eval did not work");
+//   }
+//   _ => (),
+// }
+
+// tauri::Builder::default()
+//     .plugin(tauri_plugin_global_shortcut::Builder::new().build())
+//     .plugin(tauri_plugin_opener::init())
+//     .invoke_handler(tauri::generate_handler![greet])
+//     .run(tauri::generate_context!())
+//     .expect("error while running tauri application");
