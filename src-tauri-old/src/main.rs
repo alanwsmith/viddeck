ff15beef7767ffc38738d2ec05e1f6b24ebf99f3
Alan Smith <commit@alanwsmith.com>
2026-09-15 15:39:19 -0400

    removed deprecated version

--- src-tauri-old/src/main.rs
+++
@@ -1,9 +0,0 @@
-// Prevents additional console window on Windows in release, DO NOT REMOVE!!
-#![cfg_attr(
-  not(debug_assertions),
-  windows_subsystem = "windows"
-)]
-
-fn main() {
-  youtube_scrubber_lib::run()
-}
